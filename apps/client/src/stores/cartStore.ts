077a912a6d06191726a14960718e5018501ac9c5
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-12-29 10:47:42 -0500

    setup client app

---
+++ apps/client/src/stores/cartStore.ts
@@ -0,0 +1,91 @@
+//import { CartStoreActionsType, CartStoreStateType } from "@repo/types";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+// export type CartItemType = Product & {
+//   quantity: number;
+//   selectedSize: string;
+//   selectedColor: string;
+// };
+
+export type CartItemType = {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  quantity: number;
+  selectedSize?: string;
+  selectedColor?: string;
+};
+
+export type CartItemsType = CartItemType[];
+
+export type CartStoreStateType = {
+  cart: CartItemsType;
+  hasHydrated: boolean;
+};
+
+export type CartStoreActionsType = {
+  addToCart: (product: CartItemType) => void;
+  removeFromCart: (product: CartItemType) => void;
+  clearCart: () => void;
+};
+
+const useCartStore = create<CartStoreStateType & CartStoreActionsType>()(
+  persist(
+    (set) => ({
+      cart: [],
+      hasHydrated: false,
+      addToCart: (product) =>
+        set((state) => {
+          const existingIndex = state.cart.findIndex(
+            (p) =>
+              p.id === product.id &&
+              p.selectedSize === product.selectedSize &&
+              p.selectedColor === product.selectedColor
+          );
+
+          if (existingIndex !== -1) {
+            const updatedCart = [...state.cart];
+            updatedCart[existingIndex]!.quantity += product.quantity || 1;
+            return { cart: updatedCart };
+          }
+
+          return {
+            cart: [
+              ...state.cart,
+              {
+                ...product,
+                quantity: product.quantity || 1,
+                selectedSize: product.selectedSize,
+                selectedColor: product.selectedColor,
+              },
+            ],
+          };
+        }),
+      removeFromCart: (product) =>
+        set((state) => ({
+          cart: state.cart.filter(
+            (p) =>
+              !(
+                p.id === product.id &&
+                p.selectedSize === product.selectedSize &&
+                p.selectedColor === product.selectedColor
+              )
+          ),
+        })),
+      clearCart: () => set({ cart: [] }),
+    }),
+    {
+      name: "cart",
+      storage: createJSONStorage(() => localStorage),
+      onRehydrateStorage: () => (state) => {
+        if (state) {
+          state.hasHydrated = true;
+        }
+      },
+    }
+  )
+);
+
+export default useCartStore;
