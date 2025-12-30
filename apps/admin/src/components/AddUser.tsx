105f93c48b58c2fd76a8355dfe3362bb813a88f1
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-12-29 20:46:07 -0500

    add AddUser

---
+++ apps/admin/src/components/AddUser.tsx
@@ -0,0 +1,202 @@
+"use client";
+
+import {
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "./ui/button";
+import { useAuth } from "@clerk/nextjs";
+import { useMutation } from "@tanstack/react-query";
+// import { UserFormSchema } from "@repo/types";
+import { toast } from "react-toastify";
+
+export const UserFormSchema = z.object({
+  firstName: z
+    .string({ message: "First name is required!" })
+    .min(2, { message: "First name must be at least 2 characters!" })
+    .max(50),
+  lastName: z
+    .string({ message: "Last name is required!" })
+    .min(2, { message: "Last name must be at least 2 characters!" })
+    .max(50),
+  username: z
+    .string({ message: "Username is required!" })
+    .min(2, { message: "Username must be at least 2 characters!" })
+    .max(50),
+  emailAddress: z.array(z.string({ message: "Email address is required!" })),
+  password: z
+    .string({ message: "Password is required!" })
+    .min(8, { message: "Password must be at least 8 characters!" })
+    .max(50),
+});
+
+const AddUser = () => {
+  const form = useForm<z.infer<typeof UserFormSchema>>({
+    resolver: zodResolver(UserFormSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      emailAddress: [],
+      username: "",
+      password: "",
+    },
+  });
+
+  const { getToken } = useAuth();
+
+  const mutation = useMutation({
+    mutationFn: async (data: z.infer<typeof UserFormSchema>) => {
+      const token = await getToken();
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/users`,
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (!res.ok) {
+        throw new Error("Failed to create user!");
+      }
+    },
+    onSuccess: () => {
+      toast.success("User created successfully");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return (
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle className="mb-4">Add User</SheetTitle>
+        <SheetDescription asChild>
+          <Form {...form}>
+            <form
+              className="space-y-8"
+              onSubmit={form.handleSubmit((data) => mutation.mutate(data))}
+            >
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>First Name</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormDescription>Enter user first name.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormDescription>Enter user last name.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormDescription>Enter username.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="emailAddress"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email Addresses</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="email1@gmail.com, email2@gmail.com"
+                        onChange={(e) => {
+                          const emails = e.target.value
+                            .split(",")
+                            .map((email) => email.trim())
+                            .filter((email) => email);
+                          field.onChange(emails);
+                        }}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Only admin can see your email.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="password" />
+                    </FormControl>
+                    <FormDescription>Enter user password.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                disabled={mutation.isPending}
+                className="disabled:opacity-50 disabled:cursor-not-allowed"
+              >
+                {mutation.isPending ? "Submitting..." : "Submit"}
+              </Button>
+            </form>
+          </Form>
+        </SheetDescription>
+      </SheetHeader>
+    </SheetContent>
+  );
+};
+
+export default AddUser;
