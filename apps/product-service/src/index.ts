23fae3ab08f9a4789e70a18d44985ae1d520921b
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-01 00:32:59 -0500

    update product-service

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3002", "http://localhost:3003"],
+    credentials: true,
+  })
+);
 
 app.listen(8000, () => {
   console.log("Product service is running on port 8000");
