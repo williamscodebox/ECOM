05c33c4446fc83c7918e70239253812e7c999f1c
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-01 09:58:32 -0500

    add payment-service

---
+++ apps/payment-service/src/index.ts
@@ -0,0 +1,26 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+const start = async () => {
+  try {
+    serve(
+      {
+        fetch: app.fetch,
+        port: 8002,
+      },
+      (info) => {
+        console.log(`Payment service is running on port 8002`);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+start();
