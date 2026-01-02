d1d3130acf34d894a988c894b1a333b654ae8ed4
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-01 21:56:40 -0500

    update clerk in client Navbar

--- apps/client/src/components/Navbar.tsx
+++ apps/client/src/components/Navbar.tsx
@@ -37,11 +37,6 @@ const Navbar = () => {
         <ShoppingCartIcon />
         <SignedOut>
           <SignInButton />
-          <SignUpButton>
-            <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-              Sign Up
-            </button>
-          </SignUpButton>
         </SignedOut>
         <SignedIn>
           <UserButton />
