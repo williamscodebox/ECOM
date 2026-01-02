45ffd9d7fb54289bb2253e56f5cec9b7dddd3653
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-02 17:42:16 -0500

    update product-db

--- packages/product-db/src/client.ts
+++ packages/product-db/src/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from ".././generated/prisma/client.js";
+import { PrismaClient } from ".././generated/prisma";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
