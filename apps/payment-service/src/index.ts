fad1e0bc6655200538571233b7d614ccaaf33ed4
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-03 13:29:47 -0500

    update payment-service

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -2,6 +2,7 @@ import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { shouldBeUser } from "./middleware/authMiddleware.js";
+import stripe from "./utils/stripe.js";
 
 const app = new Hono();
 
@@ -25,6 +26,27 @@ app.get("/test", shouldBeUser, (c) => {
     userId: c.get("userId"),
   });
 });
+
+app.post("/create-stripe-product", async (c) => {
+  const res = await stripe.products.create({
+    id: "123",
+    name: "Test Product",
+    default_price_data: {
+      currency: "usd",
+      unit_amount: 10 * 100,
+    },
+  });
+
+  return c.json(res);
+});
+
+// app.get("/stripe-product-price", async (c) => {
+//   const res = await stripe.prices.list({
+//     product: "123",
+//   });
+
+//   return c.json(res);
+// });
 
 // app.get("/pay", shouldBeUser, async (c) => {
 //   const { products } = await c.req.json();
