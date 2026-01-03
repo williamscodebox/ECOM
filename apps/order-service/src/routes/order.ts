5543101d6644b11c98b8b9a26ffd6253144dddb2
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-03 12:03:01 -0500

    update order-service

--- apps/order-service/src/routes/order.ts
+++ apps/order-service/src/routes/order.ts
@@ -1,8 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { shouldBeAdmin, shouldBeUser } from "../middleware/authMiddleware";
 import { Order } from "@repo/order-db";
-//import { startOfMonth, subMonths } from "date-fns";
-//import { OrderChartType } from "@repo/types";
+import { startOfMonth, subMonths } from "date-fns";
+import { OrderChartType } from "@repo/types";
 
 export const orderRoute = async (fastify: FastifyInstance) => {
   fastify.get(
@@ -22,89 +22,89 @@ export const orderRoute = async (fastify: FastifyInstance) => {
       return reply.send(orders);
     }
   );
-  //   fastify.get(
-  //     "/order-chart",
-  //     { preHandler: shouldBeAdmin },
-  //     async (request, reply) => {
-  //       const now = new Date();
-  //       const sixMonthsAgo = startOfMonth(subMonths(now, 5));
+  fastify.get(
+    "/order-chart",
+    { preHandler: shouldBeAdmin },
+    async (request, reply) => {
+      const now = new Date();
+      const sixMonthsAgo = startOfMonth(subMonths(now, 5));
 
-  //       // { month: "April", total: 173, successful: 100 }
+      // { month: "April", total: 173, successful: 100 }
 
-  //       const raw = await Order.aggregate([
-  //         {
-  //           $match: {
-  //             createdAt: { $gte: sixMonthsAgo, $lte: now },
-  //           },
-  //         },
-  //         {
-  //           $group: {
-  //             _id: {
-  //               year: { $year: "$createdAt" },
-  //               month: { $month: "$createdAt" },
-  //             },
-  //             total: { $sum: 1 },
-  //             successful: {
-  //               $sum: {
-  //                 $cond: [{ $eq: ["$status", "success"] }, 1, 0],
-  //                 // {
-  //                 //   "year":2025,
-  //                 //   "month":9,
-  //                 //   "total":100,
-  //                 //   "successful":72
-  //                 // }
-  //               },
-  //             },
-  //           },
-  //         },
-  //         {
-  //           $project: {
-  //             _id: 0,
-  //             year: "$_id.year",
-  //             month: "$_id.month",
-  //             total: 1,
-  //             successful: 1,
-  //           },
-  //         },
-  //         {
-  //           $sort: { year: 1, month: 1 },
-  //         },
-  //       ]);
+      const raw = await Order.aggregate([
+        {
+          $match: {
+            createdAt: { $gte: sixMonthsAgo, $lte: now },
+          },
+        },
+        {
+          $group: {
+            _id: {
+              year: { $year: "$createdAt" },
+              month: { $month: "$createdAt" },
+            },
+            total: { $sum: 1 },
+            successful: {
+              $sum: {
+                $cond: [{ $eq: ["$status", "success"] }, 1, 0],
+                // {
+                //   "year":2025,
+                //   "month":9,
+                //   "total":100,
+                //   "successful":72
+                // }
+              },
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            year: "$_id.year",
+            month: "$_id.month",
+            total: 1,
+            successful: 1,
+          },
+        },
+        {
+          $sort: { year: 1, month: 1 },
+        },
+      ]);
 
-  //       const monthNames = [
-  //         "January",
-  //         "February",
-  //         "March",
-  //         "April",
-  //         "May",
-  //         "June",
-  //         "July",
-  //         "August",
-  //         "September",
-  //         "October",
-  //         "November",
-  //         "December",
-  //       ];
+      const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
 
-  //       const results: OrderChartType[] = [];
+      const results: OrderChartType[] = [];
 
-  //       for (let i = 5; i >= 0; i--) {
-  //         const d = subMonths(now, i);
-  //         const year = d.getFullYear();
-  //         const month = d.getMonth() + 1;
+      for (let i = 5; i >= 0; i--) {
+        const d = subMonths(now, i);
+        const year = d.getFullYear();
+        const month = d.getMonth() + 1;
 
-  //         const match = raw.find(
-  //           (item) => item.year === year && item.month === month
-  //         );
+        const match = raw.find(
+          (item) => item.year === year && item.month === month
+        );
 
-  //         results.push({
-  //           month: monthNames[month - 1] as string,
-  //           total: match ? match.total : 0,
-  //           successful: match ? match.successful : 0,
-  //         });
-  //       }
+        results.push({
+          month: monthNames[month - 1] as string,
+          total: match ? match.total : 0,
+          successful: match ? match.successful : 0,
+        });
+      }
 
-  //       return reply.send(results);
-  //     }
-  //   );
+      return reply.send(results);
+    }
+  );
 };
