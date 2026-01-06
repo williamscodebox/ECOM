05726bf3409609909e09ca6df8222996d84f9b81
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-06 10:48:40 -0500

    update payment-service

---
+++ apps/payment-service/src/utils/subscrptions.ts
@@ -0,0 +1,54 @@
+import { consumer } from "./kafka";
+import { createStripeProduct, deleteStripeProduct } from "./stripeProduct";
+
+export const runKafkaSubscriptions = async () => {
+  // consumer.subscribe("product.created", async (message) => {
+  //   const product = message.value;
+  //   console.log("Received message: product.created", product);
+
+  //   await createStripeProduct(product);
+  // });
+  // consumer.subscribe("product.deleted", async (message) => {
+  //   const productId = message.value;
+  //   console.log("Received message: product.deleted", productId);
+
+  //   await deleteStripeProduct(productId);
+  // });
+
+  //   consumer.subscribe([
+  //     {
+  //       topicName: "product.created",
+  //       topicHandler: async (message) => {
+  //         const product = message.value;
+  //         console.log("Received message: product.created", product);
+
+  //         await createStripeProduct(product);
+  //       },
+  //     },
+  //     {
+  //       topicName: "product.deleted",
+  //       topicHandler: async (message) => {
+  //         const productId = message.value;
+  //         console.log("Received message: product.deleted", productId);
+
+  //         await deleteStripeProduct(productId);
+  //       },
+  //     },
+  //   ]);
+  consumer.subscribe([
+    {
+      topicName: "product.created",
+      topicHandler: async (product) => {
+        console.log("Received message: product.created", product);
+        await createStripeProduct(product);
+      },
+    },
+    {
+      topicName: "product.deleted",
+      topicHandler: async (productId) => {
+        console.log("Received message: product.deleted", productId);
+        await deleteStripeProduct(productId);
+      },
+    },
+  ]);
+};
