c0c79c3bc9823c8170747e5d5837aa2179ee7800
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-06 17:28:32 -0500

    update payment-service

--- apps/payment-service/src/utils/subscrptions.ts
+++ apps/payment-service/src/utils/subscrptions.ts
@@ -2,19 +2,6 @@ import { consumer } from "./kafka";
 import { createStripeProduct, deleteStripeProduct } from "./stripeProduct";
 
 export const runKafkaSubscriptions = async () => {
-  // consumer.subscribe("product.created", async (message) => {
-  //   const product = message.value;
-  //   console.log("Received message: product.created", product);
-
-  //   await createStripeProduct(product);
-  // });
-  // consumer.subscribe("product.deleted", async (message) => {
-  //   const productId = message.value;
-  //   console.log("Received message: product.deleted", productId);
-
-  //   await deleteStripeProduct(productId);
-  // });
-
   consumer.subscribe([
     {
       topicName: "product.created",
