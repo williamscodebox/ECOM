79255745b12557aa6ed589be15bc7060e00a8bfe
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-06 17:14:25 -0500

    update payment-service

--- apps/payment-service/src/routes/webhooks.route.ts
+++ apps/payment-service/src/routes/webhooks.route.ts
@@ -31,33 +31,33 @@ webhookRoute.post("/stripe", async (c) => {
     case "payment_intent.succeeded":
       const paymentIntent = event.data.object as Stripe.PaymentIntent;
 
-      const cart = JSON.parse(paymentIntent.metadata.cart ?? "");
+      //const cart = JSON.parse(paymentIntent.metadata.cart ?? "");
       const products = JSON.parse(paymentIntent.metadata.cart ?? "[]");
       const userId = paymentIntent.metadata.userId; // Now you have your line items console.log(cart);
       const email = paymentIntent.receipt_email;
       const amount = (paymentIntent.amount_received / 100).toFixed(2);
-      const status = paymentIntent.status;
+      // const status = paymentIntent.status;
 
       // Create order using Kafka
 
-      // producer.send("payment.successful", {
-      //   value: {
-      //     userId: userId,
-      //     email: paymentIntent.metadata.email,
-      //     amount: paymentIntent.metadata.amount_total,
-      //     status:
-      //       paymentIntent.metadata.payment_status === "succeeded"
-      //         ? "success"
-      //         : "failed",
-      //     products: products?.map((item: any) => ({
-      //       name: item.description,
-      //       quantity: item.quantity,
-      //       price: item.price?.unit_amount,
-      //     })),
-      //   },
-      // });
+      producer.send("payment.successful", {
+        value: {
+          userId: userId,
+          email: email,
+          amount: amount,
+          status:
+            paymentIntent.metadata.payment_status === "succeeded"
+              ? "success"
+              : "failed",
+          products: products?.map((item: any) => ({
+            name: item.description,
+            quantity: item.quantity,
+            price: item.price?.unit_amount,
+          })),
+        },
+      });
 
-      console.log("Webhook Received", userId, products, email, amount, status);
+      //console.log("Webhook Received", userId, products, email, amount, status);
 
       break;
 
