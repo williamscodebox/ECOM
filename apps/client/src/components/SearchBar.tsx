b9546513d6ed3dd03ca408c33f4060a7ca63bd9f
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-11 19:53:52 -0500

    update SearchBar

--- apps/client/src/components/SearchBar.tsx
+++ apps/client/src/components/SearchBar.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { Search } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 
 const SearchBar = () => {
   const [value, setValue] = useState("");
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
   return (
     <div className="hidden sm:flex items-center gap-2 rounded-md ring-1 ring-gray-200 px-2 py-1 shadow-md">
