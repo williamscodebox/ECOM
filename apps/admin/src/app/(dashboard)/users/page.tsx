fd059bb4092e91eff85d1743105911b1308606ac
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-13 16:27:07 -0500

    update admin users page

--- apps/admin/src/app/(dashboard)/users/page.tsx
+++ apps/admin/src/app/(dashboard)/users/page.tsx
@@ -1,9 +1,8 @@
-import type { User } from "@clerk/nextjs/server";
+import { auth, type User } from "@clerk/nextjs/server";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 
-const getData = async (): Promise<User[]> => {
-  return [];
+const getData = async (): Promise<{ data: User[]; totalCount: number }> => {
   // return [
   //   {
   //     id: "728ed521",
@@ -258,16 +257,37 @@ const getData = async (): Promise<User[]> => {
   //     email: "annecruz@gmail.com",
   //   },
   // ];
+
+  const { getToken } = await auth();
+  const token = await getToken();
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/users`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+    return { data: [], totalCount: 0 };
+  }
 };
 
 const UsersPage = async () => {
-  const data = await getData();
+  const res = await getData();
   return (
     <div className="">
       <div className="mb-8 px-4 py-2 bg-secondary rounded-md">
         <h1 className="font-semibold">All Users</h1>
       </div>
-      <DataTable columns={columns} data={data ?? []} />
+      <DataTable
+        columns={columns}
+        data={res.data ?? { data: [], totalCount: 0 }}
+      />
     </div>
   );
 };
