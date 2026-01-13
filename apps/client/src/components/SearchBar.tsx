f45acf2d0433869633b5e13b2e5ce806c08d354b
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-12 19:08:01 -0500

    update SearchBar

--- apps/client/src/components/SearchBar.tsx
+++ apps/client/src/components/SearchBar.tsx
@@ -9,6 +9,12 @@ const SearchBar = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
 
+  const handleSearch = (value: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("search", value);
+    router.push(`/products?${params.toString()}`, { scroll: false });
+  };
+
   return (
     <div className="hidden sm:flex items-center gap-2 rounded-md ring-1 ring-gray-200 px-2 py-1 shadow-md">
       <Search className="w-4 h-4 text-gray-500" />
@@ -16,6 +22,12 @@ const SearchBar = () => {
         id="search"
         placeholder="Search..."
         className="text-sm outline-0"
+        onChange={(e) => setValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSearch(value);
+          }
+        }}
       />
     </div>
   );
