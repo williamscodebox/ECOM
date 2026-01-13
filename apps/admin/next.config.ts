bf23e8964b47179344b9c518afcd75462529b2f9
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-13 18:32:33 -0500

    update admin app

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -8,6 +8,8 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.pexels.com",
       },
+      { protocol: "https", hostname: "img.clerk.com" },
+      { protocol: "https", hostname: "images.clerk.dev" },
     ],
   },
 };
