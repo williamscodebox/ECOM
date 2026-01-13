bd045f43ebdc7441ef0acb15eaab20ea60dd7a4e
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-13 16:17:29 -0500

    update admin users page

--- apps/admin/src/app/(dashboard)/users/columns.tsx
+++ apps/admin/src/app/(dashboard)/users/columns.tsx
@@ -11,18 +11,19 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
+import type { User } from "@clerk/nextjs/server";
 import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-export type User = {
-  id: string;
-  avatar: string;
-  fullName: string;
-  email: string;
-  status: "active" | "inactive";
-};
+// export type User = {
+//   id: string;
+//   avatar: string;
+//   fullName: string;
+//   email: string;
+//   status: "active" | "inactive";
+// };
 
 export const columns: ColumnDef<User>[] = [
   {
@@ -51,8 +52,8 @@ export const columns: ColumnDef<User>[] = [
       return (
         <div className="w-9 h-9 relative">
           <Image
-            src={user.avatar}
-            alt={user.fullName}
+            src={user.imageUrl}
+            alt={user.firstName || user.username || "-"}
             fill
             className="rounded-full object-cover"
           />
