9d1a8f4f25c9f566cba036561591ebcebce5ba0c
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-13 12:08:47 -0500

    update admin product page

--- apps/admin/src/app/(dashboard)/products/columns.tsx
+++ apps/admin/src/app/(dashboard)/products/columns.tsx
@@ -10,24 +10,25 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { cn } from "@/lib/utils";
+// import { cn } from "@/lib/utils";
+import { ProductType } from "@repo/types";
 import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-export type Product = {
-  id: string | number;
-  price: number;
-  name: string;
-  shortDescription: string;
-  description: string;
-  sizes: string[];
-  colors: string[];
-  images: Record<string, string>;
-};
+// export type Product = {
+//   id: string | number;
+//   price: number;
+//   name: string;
+//   shortDescription: string;
+//   description: string;
+//   sizes: string[];
+//   colors: string[];
+//   images: Record<string, string>;
+// };
 
-export const columns: ColumnDef<Product>[] = [
+export const columns: ColumnDef<ProductType>[] = [
   {
     id: "select",
     header: ({ table }) => (
@@ -113,7 +114,8 @@ export const columns: ColumnDef<Product>[] = [
             </DropdownMenuItem>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
-              <Link href={`/products/${product.id}`}>View customer</Link>
+              {/* <Link href={`/products/${product.id}`}>View customer</Link> */}
+              <Link href={`/products/${product.id}`}>View product</Link>
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
