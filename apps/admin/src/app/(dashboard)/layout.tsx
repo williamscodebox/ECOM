e31f5da13af124260443d2f180a908ae65e2f341
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-14 12:16:12 -0500

    add QueryProvider

--- apps/admin/src/app/(dashboard)/layout.tsx
+++ apps/admin/src/app/(dashboard)/layout.tsx
@@ -1,8 +1,10 @@
 import AppSidebar from "@/components/AppSidebar";
 import Navbar from "@/components/Navbar";
+import QueryProvider from "@/components/providers/QueryProvider";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { cookies } from "next/headers";
+import { ToastContainer } from "react-toastify";
 
 export default async function RootLayout({
   children,
@@ -13,21 +15,24 @@ export default async function RootLayout({
   const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
 
   return (
-    <div className="flex w-screen">
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <SidebarProvider defaultOpen={defaultOpen}>
-          <AppSidebar />
-          <main className="w-full">
-            <Navbar />
-            <div className="px-4">{children}</div>
-          </main>
-        </SidebarProvider>
-      </ThemeProvider>
-    </div>
+    <QueryProvider>
+      <div className="flex w-screen">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <SidebarProvider defaultOpen={defaultOpen}>
+            <AppSidebar />
+            <main className="w-full">
+              <Navbar />
+              <div className="px-4">{children}</div>
+            </main>
+          </SidebarProvider>
+        </ThemeProvider>
+      </div>
+      <ToastContainer position="bottom-right" />
+    </QueryProvider>
   );
 }
