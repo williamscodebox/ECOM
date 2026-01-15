ba93e14a6aacb997a58b146ceb6c84662b603e59
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-15 02:21:56 -0500

    update email-service

--- apps/email-service/src/utils/mailer.ts
+++ apps/email-service/src/utils/mailer.ts
@@ -11,23 +11,23 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-// const sendMail = async ({
-//   email,
-//   subject,
-//   text,
-// }: {
-//   email: string;
-//   subject: string;
-//   text: string;
-// }) => {
-//   const res = await transporter.sendMail({
-//     from: '"Lama Dev" <lamadev@gmail.com>',
-//     to: email,
-//     subject,
-//     text,
-//   });
+const sendMail = async ({
+  email,
+  subject,
+  text,
+}: {
+  email: string;
+  subject: string;
+  text: string;
+}) => {
+  const res = await transporter.sendMail({
+    from: '"Crew Phil" <crewphil45@gmail.com>',
+    to: email,
+    subject,
+    text,
+  });
 
-//   console.log("MESSAGE SENT:", res);
-// };
+  console.log("MESSAGE SENT:", res);
+};
 
-// export default sendMail;
+export default sendMail;
