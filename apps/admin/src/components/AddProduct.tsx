62239478f5eb9d71c9369f6884c4fee343f55f4e
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-01-15 01:30:04 -0500

    update AddProduct

--- apps/admin/src/components/AddProduct.tsx
+++ apps/admin/src/components/AddProduct.tsx
@@ -393,7 +393,7 @@ const AddProduct = () => {
                                       );
 
                                       const res = await fetch(
-                                        `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+                                        `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/samples/ecommerce`,
                                         {
                                           method: "POST",
                                           body: formData,
